525250f6532e2ea381e8816a11d71e919cc49a69
Edward-SI03 <tlwlsduq95@gmail.com>
2022-07-28 18:46:35 +0900

    코드 분리 /  db가 사라졌다.. 추가를 못하는 문제 발생.....

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Http = require('http');
-const socketIo = require('socket.io');
+
 const app = express();
 // http가 express 서버를 상속받아서 확장해줌
 const http = Http.createServer(app);
@@ -17,50 +17,6 @@ const router = express.Router();
 //     console.log("미들웨어 사용")
 //     next()
 // })
-
-const io = socketIo(http);
-let socketIdMap = {};
-function samePageCount() {
-  const countUrl = Object.values(socketIdMap).reduce((value, url) => {
-    return {
-      ...value,
-      [url]: value[url] ? value[url] + 1 : 1,
-    };
-  }, {});
-  console.log(countUrl);
-
-  for (const [socketId, url] of Object.entries(socketIdMap)) {
-    const count = countUrl[url];
-    io.to(socketId).emit('SAME_PAGE_VIEWER_COUNT', count);
-  }
-}
-
-io.on('connection', (socket) => {
-  // 클라가 BUY로 이벤트를 실행하면 data를 받겠다
-  socket.on('BUY', (data) => {
-    const payload = {
-      nickname: data.nickname,
-      goodsId: data.goodsId,
-      goodsName: data.goodsName,
-      date: new Date().toISOString(),
-    };
-    console.log('클라가 구매한 데이터', data);
-    // 나를 포함한 모두에게 BUY_GOODS라는 이벤트에 payload 데이터를 전송
-    // io.emit("BUY_GOODS", payload)
-    // 나를 제외한 모두에게 BUY_GOODS라는 이벤트에 payload 데이터를 전송
-    socket.broadcast.emit('BUY_GOODS', payload);
-  });
-
-  socket.on('CHANGED_PAGE', (data) => {
-    socketIdMap[socket.id] = data;
-    samePageCount();
-  });
-
-  socket.on('disconnect', () => {
-    delete socketIdMap[socket.id];
-    samePageCount();
-  });
-});
 
 // const requstMiddleware = (req, res, next) => {
 //   console.log("request : ", req.originalUrl, " - ", new Date());
@@ -88,6 +44,8 @@ app.get('/', (req, res) => {
 });
 
 // listen은 express로 만든 웹서버를 실행시켜줌
-http.listen(port, () => {
-  console.log(port, '포트로 서버가 열렸어요!');
-});
+// http.listen(port, () => {
+//   console.log(port, '포트로 서버가 열렸어요!');
+// });
+
+module.exports = http
